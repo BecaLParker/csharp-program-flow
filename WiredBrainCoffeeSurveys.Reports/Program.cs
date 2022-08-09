cf038b68dc21159a074d2ea374cbfab70876d697
Beca Parker <beca.parker@red-gate.com>
2022-08-09 13:31:14 +0100

    Print overall score to console

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -9,8 +9,11 @@ namespace WiredBrainCoffeeSurveys.Reports
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
             double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
 
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
             Console.WriteLine($"Response Percentage: {responseRate}");
             Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
+            Console.WriteLine($"Overall Score: {overallScore}");
         }
     }
 }
